2daf5285f35a7345e1f496dae0866104b03ac98e
Michał Adamczyk <adamczyk888@gmail.com>
2018-05-15 21:57:10 +0200

    Zadanie 17.6 Poprawione v.2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var apiRoot = 'https://salty-island-63707.herokuapp.com/v1/task';
+  var apiRoot = 'https://salty-island-63707.herokuapp.com/v1/task/';
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   var tasksContainer = $('[data-tasks-container]');
 
